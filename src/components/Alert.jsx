b2cbb934df0f784d105fd5f3c90125fb0c3e0fb7
SFAdmin <admin@seminarfoundry.com>
2023-01-02 16:01:50 -0500

    Removed AnimatePresence

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -1,4 +1,4 @@
-import { motion, AnimatePresence } from 'framer-motion';
+import { motion } from 'framer-motion';
 
 function Alert({ id, message, setShowCharAlert, setShowCopyAlert, setShowEmptyAlert }) {
     setTimeout(() => {
@@ -8,23 +8,21 @@ function Alert({ id, message, setShowCharAlert, setShowCopyAlert, setShowEmptyAl
     }, 2000);
 
     return (
-        <AnimatePresence>
-            <motion.div
-                className="alert-container"
-                key={id}
-                initial={{ y: 50, opacity: 0 }}
-                animate={{ y: -52, opacity: 1 }}
-                exit={{ opacity: 0 }}
-                transition={{
-                    type: 'spring',
-                    damping: 15
-                }}
-            >
-                <div className="alert">
-                    <p>{message}</p>
-                </div>
-            </motion.div>
-        </AnimatePresence>
+        <motion.div
+            className="alert-container"
+            key={id}
+            initial={{ y: 50, opacity: 0 }}
+            animate={{ y: -52, opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{
+                type: 'spring',
+                damping: 15
+            }}
+        >
+            <div className="alert">
+                <p>{message}</p>
+            </div>
+        </motion.div>
     );
 }
 
