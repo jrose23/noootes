971f3e879ce2b238d5007b917a2b890325a05915
SFAdmin <admin@seminarfoundry.com>
2023-01-07 17:02:15 -0500

    Working on empty note message

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,8 +1,21 @@
+import { motion } from 'framer-motion';
+import NoteFella from '../assets/Note-Fella.svg';
+
 function Message() {
     return (
-        <div className="container message">
-            <h1>No notes...</h1>
-        </div>
+        <motion.div
+            className="message"
+            initial={{ y: 100, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{
+                type: 'spring',
+                damping: 15
+            }}
+        >
+            <img className="message-img" src={NoteFella} />
+            <h1>Let's make some notes!</h1>
+            <h3>Create one now</h3>
+        </motion.div>
     );
 }
 
